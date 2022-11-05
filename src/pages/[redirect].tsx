37fa694f4c0b7412a18e2c92e881c46f5e9a9ffb
ChrisCoy <christopherleegs@outlook.com>
2022-11-04 23:20:03 -0300

    feat: redirect route

--- src/pages/[redirect].tsx
+++ src/pages/[redirect].tsx
@@ -1,15 +1,42 @@
-export default function index({ teste }: any) {
-  return <div>{teste}</div>;
+import { useRouter } from "next/router";
+import { formatLink } from "src/util/formatLink";
+import productsJson from "@mocs/products.json";
+import { useEffect } from "react";
+
+interface RedirectProps {
+  link: string;
+  type: string;
+}
+
+export default function Redirect({ link, type }: RedirectProps) {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (link) {
+      push(`/${type}/${formatLink(link)}`);
+    } else {
+      push(`/404`);
+    }
+  }, []);
+
+  return null;
 }
 
 export const getServerSideProps = async ({ req, params }: any) => {
   try {
     const { redirect } = params;
 
-    const link = null;
+    let link = null;
+
+    link = productsJson.find((prod) => prod.link == redirect)?.link;
+    if (link) {
+      return {
+        props: { type: "produtos", link },
+      };
+    }
 
     return {
-      props: { link },
+      props: { type: null, link: null },
     };
   } catch (error) {
     console.error({ error });
