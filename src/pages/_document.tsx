3b1bb00676d5a88bc5cb5e2e7bcb173cea3a5def
Carlos Augusto <auggustus6@gmail.com>
2024-05-17 17:26:32 -0300

    chore: Add tags google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,23 +43,28 @@ export default class MyDocument extends Document {
             type="image/x-icon"
           />
 
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-ZR8W8Y0XFW"
-          ></script>
           <script>
             {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-ZR8W8Y0XFW');
+            function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N5MHZGG7');
             `}
           </script>
         </Head>
         <body>
           <Main />
           <NextScript />
+
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-N5MHZGG7"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
         </body>
       </Html>
     );
